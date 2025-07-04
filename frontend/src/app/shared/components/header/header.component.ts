ac57cd57e40dfa826d0248c039e45876e452ae52
jasper <jasper20467@hotmail.com>
2025-07-04 12:03:54 +0800

    feat: fix frontend RWD issue

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   appName = '家用智慧助理系統';
+  mobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.mobileMenuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    const headerElement = target.closest('.header');
+    
+    if (!headerElement && this.mobileMenuOpen) {
+      this.closeMobileMenu();
+    }
+  }
 }
